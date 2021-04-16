4926740d427c15551ec9b3947929e79426e109ca
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-04-16 16:00:45 -0600

    Add object variables

--- model-1.js
+++ model-1.js
@@ -9,5 +9,10 @@ var hasDog = true;
 var isWorking = true;
 var hasCat = false;
 
-var favoriteFoods = ["Pasta", "Eggs", "Pho"]
-var countriesVisited = ["Canada", "Germany", "Belgium"]
+var favoriteFoods = ["Pasta", "Eggs", "Pho"];
+var countriesVisited = ["Canada", "Germany", "Belgium"];
+
+var dog = {name: "Mister Snuffles", age: 3, breed: "Corgi", color: ["White", "Tan", "Black"]};
+var person = {name: firstName + lastName, age: age, favoriteFoods: favoriteFoods, favoriteNumber: favoriteNumber, dog: dog};
+
+console.log(person);
