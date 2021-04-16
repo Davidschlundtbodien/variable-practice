0ed7c5554107e0ecde7b22830a67d25e10d04aeb
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-04-16 16:02:15 -0600

    Add comments for each variable type

--- model-1.js
+++ model-1.js
@@ -1,18 +1,17 @@
+//STRING
 var firstName = "Bob";
 var lastName = "Smith";
 var job = "Developer";
-
+//NUMBER
 var age = 30;
 var favoriteNumber = 24;
-
+//BOOLEAN
 var hasDog = true;
 var isWorking = true;
 var hasCat = false;
-
+//ARRAY
 var favoriteFoods = ["Pasta", "Eggs", "Pho"];
 var countriesVisited = ["Canada", "Germany", "Belgium"];
-
+//OBJECT
 var dog = {name: "Mister Snuffles", age: 3, breed: "Corgi", color: ["White", "Tan", "Black"]};
 var person = {name: firstName + lastName, age: age, favoriteFoods: favoriteFoods, favoriteNumber: favoriteNumber, dog: dog};
-
-console.log(person);
